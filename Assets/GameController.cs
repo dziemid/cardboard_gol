44ce9bf4f226415a491fe1c72f3a83eb128f2217
Greg Dziemidowicz <greg@cogent.co>
2015-11-10 07:23:00 +1100

    initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,206 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject cell;
+	public GameObject explosion;
+
+	private GameObject[][] board;
+
+	private float nextActionTime = 5.0f;
+
+	public float period = 0.1f;
+
+	private int boardSize = 50;
+
+	void Start () {
+		board = createBoard ();
+
+		addSquare (0, 0);
+		addSquare (0, boardSize-5);
+		addSquare (boardSize-5, boardSize-5);
+		addSquare (boardSize-5, 0);
+
+		addSquare (23, 23);
+		addSquare (25, 25);
+
+		addCell(board, 3,5);
+		addCell(board, 3,6);
+		addCell(board, 4,5);
+
+		addCell(board, 6,7);
+		addCell(board, 6,8);
+		addCell(board, 5,8);
+
+
+		addCell(board, 25,5);
+		addCell(board, 25,6);
+		addCell(board, 25,7);
+
+		addCell(board, 35,30);
+		addCell(board, 35,31);
+		addCell(board, 35,32);
+
+		addCell(board, 25,40);
+		addCell(board, 25,41);
+		addCell(board, 25,42);
+
+
+
+
+		addCell(board, 10,10);
+		addCell(board, 11,10);
+		addCell(board, 12,10);
+
+		addCell(board, 10,11);
+		addCell(board, 12,11);
+
+		addCell(board, 10,12);
+		addCell(board, 11,12);
+		addCell(board, 12,12);
+
+		addCell(board, 10,13);
+		addCell(board, 11,13);
+		addCell(board, 12,13);
+
+		addCell(board, 10,14);
+		addCell(board, 11,14);
+		addCell(board, 12,14);
+
+
+		addCell(board, 10,15);
+		addCell(board, 11,15);
+		addCell(board, 12,15);
+
+		addCell(board, 10,16);
+		addCell(board, 12,16);
+
+
+		addCell(board, 10,17);
+		addCell(board, 11,17);
+		addCell(board, 12,17);
+
+		// glider
+		addGlider (20, 10);
+
+		addGlider (30, 4);
+
+
+		addGlider (40, 15);
+
+	}
+
+	void addGlider(int x, int y) {
+		addCell(board, x+3,y+1);
+		addCell(board, x+1,y+2);
+		addCell(board, x+3,y+2);
+		addCell(board, x+2,y+3);
+		addCell(board, x+3,y+3);
+
+	}
+
+	void addSquare(int x, int y) {
+		addCell(board, x,y);
+		addCell(board, x+1,y);
+		addCell(board, x,y+1);
+	}
+	
+	void Update () {
+
+		if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+			updateBoard();
+		}
+	
+	}
+
+	GameObject[][] createBoard() {
+
+		GameObject[][] newBoard = new GameObject[boardSize][];
+		for (int i=0; i < boardSize; i++) {
+			newBoard[i] = new GameObject[boardSize];
+		};
+		return newBoard;
+	}
+
+	void updateBoard() {
+		GameObject[][] newBoard = createBoard ();
+		for (int i=0; i < boardSize; i++) {
+			for (int j=0; j < boardSize; j++) {
+				int neighboursCount = aliveNeighbours(board, i, j);
+
+				if (board[i][j] && (neighboursCount == 2 || neighboursCount == 3)) {
+					addCell(newBoard, i, j);
+				}
+
+				else if (board[i][j] == null && neighboursCount == 3) {
+					addCell(newBoard, i, j);
+				}
+				else if (board[i][j])  {
+					Instantiate (explosion, board[i][j].transform.position, board[i][j].transform.rotation);
+				}
+
+			}
+		}
+
+		replaceBoard (newBoard);
+	}
+
+	int aliveNeighbours(GameObject[][] board, int x, int y) {
+		int neighboursCount = 0;
+
+		if (y - 1 >= 0) {
+			if (x - 1 >= 0 && board [x - 1] [y - 1]) {
+				neighboursCount++;
+			}
+			if (board [x] [y - 1]) {
+				neighboursCount++;
+			}
+			if (x + 1 < boardSize && board [x + 1] [y - 1]) {
+				neighboursCount++;
+			}
+		}
+
+		if (x - 1 >= 0 && board[x-1][y]) {
+			neighboursCount++;
+		}
+		if (x + 1 < boardSize && board[x+1][y]) {
+			neighboursCount++;
+		}
+
+		if (y + 1 < boardSize) {
+			if (x - 1 >= 0 && board [x - 1] [y + 1]) {
+				neighboursCount++;
+			}
+			if (board [x] [y + 1]) {
+				neighboursCount++;
+			}
+			if (x + 1 < boardSize && board [x + 1] [y + 1]) {
+				neighboursCount++;
+			}
+		}
+
+		return neighboursCount;
+	}
+
+	void replaceBoard(GameObject[][] newBoard) {
+
+		for (int i=0; i < boardSize; i++) {
+			for (int j=0; j < boardSize; j++) {
+				if (board[i][j]) {
+					Destroy(board[i][j]);
+				}
+			}
+		}
+
+		board = newBoard;
+	}
+
+	void addCell(GameObject[][] aBoard, int x, int z) {
+		GameObject newcell = Instantiate<GameObject>(cell);
+		newcell.transform.position = new Vector3(x, 1, z);
+
+		aBoard [x] [z] = newcell;
+	}
+}
